6a4b33cb2ab5a4fecd24d928102fa924a9b2805a
Ishandhananjaya <dhananjayaishan21@gmail.com>
2026-07-06 14:25:55 +0530

    Create evaluations.js
    Import Evaluation Dependencies

---
+++ controller/admin/evaluation/evaluations.js
@@ -0,0 +1,5 @@
+const Evaluation = require('../../models/Evaluation');
+const ActivityLog = require('../../models/ActivityLog');
+const Notification = require('../../models/Notification');
+const Project = require('../../models/Project');
+const StudentResearch = require('../../models/StudentResearch');
