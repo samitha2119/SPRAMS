54d401ddc4c2deaa31c42549bd905a7e1594f35b
Ishandhananjaya <dhananjayaishan21@gmail.com>
2026-07-06 14:17:02 +0530

    Update Notification.js
    Add Resource References

--- notification/server/src/models/Notification.js
+++ notification/server/src/models/Notification.js
@@ -1,13 +1,23 @@
-title: {
+link: {
     type: String,
-    required: [true, 'Title is required'],
     trim: true,
-    maxlength: [200, 'Title cannot exceed 200 characters'],
+    default: null,
 },
 
-message: {
+relatedId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: null,
+},
+
+relatedModel: {
     type: String,
-    required: [true, 'Message is required'],
-    trim: true,
-    maxlength: [1000, 'Message cannot exceed 1000 characters'],
+    enum: [
+        'Project',
+        'StudentResearch',
+        'LecturerResearch',
+        'Evaluation',
+        'FormTemplate',
+        null,
+    ],
+    default: null,
 },
