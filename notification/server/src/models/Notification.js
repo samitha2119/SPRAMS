ceafad0f2405344a71e4ab28a292ce64c3bee4b5
Ishandhananjaya <dhananjayaishan21@gmail.com>
2026-07-06 14:19:24 +0530

    Update Notification.js
    export notification model

--- notification/server/src/models/Notification.js
+++ notification/server/src/models/Notification.js
@@ -1,14 +1,1 @@
-notificationSchema.index({
-    recipientId: 1,
-    isRead: 1,
-    createdAt: -1,
-});
-
-notificationSchema.index({
-    recipientId: 1,
-    createdAt: -1,
-});
-
-notificationSchema.index({
-    type: 1,
-});
+module.exports = mongoose.model('Notification', notificationSchema);
