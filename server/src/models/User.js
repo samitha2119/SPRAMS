5e79b1d4459a4fb1bfbb887a80bdabc10dc2a7b6
ishan dhananjaya <dhananjayaishan21@gmail.com>
2026-07-06 16:32:23 +0530

    Dev (#36)
    * profect.js
    
    create project.js
    
    * edit workspace
    
    * edit student scheama
    
    edit student schema
    
    * evaluationpage.jsx
    
    creating
    
    * Create Untitled-2.js
    
    edit project schema
    
    * Create Untitled-3.js
    
    edit project group schema
    
    * evaluationpage.jsx
    
    updated
    
    * Create Untitled-4.js
    
    edit submission schema
    
    * workspace.js
    
    edit file
    
    * evaluationpage.jsx
    
    update
    
    * edit
    
    edit
    
    * evaluationpage.jsx
    
    update
    
    * edit
    
    edit file
    
    * LecturerResearchPage.jsx
    
    create
    
    * LecturerResearchPage.jsx
    
    update
    
    * lecturerResearchpage.jsx
    
    update
    
    * lecturerResearchpage
    
    update
    
    * lecturerResearchpage.jsx
    
    update
    
    * Create Notification.js
    
    edit notification schema
    
    * Update Notification.js
    
    add recipient and sender fields
    
    * Update Notification.js
    
    add notification type
    
    * Update Notification.js
    
    Add Notification Content
    
    * Update Notification.js
    
    Add Resource References
    
    * Update Notification.js
    
    add read status
    
    * Update Notification.js
    
    add database index
    
    * Update Notification.js
    
    export notification model
    
    * Create evaluations.js
    
    Import Evaluation Dependencies
    
    * Update evaluations.js
    
    Add Get Evaluations Endpoint
    
    * Update evaluations.js
    
    Add Get Single Evaluation
    
    * Update evaluations.js
    
    Add Create Evaluation
    
    * Update evaluations.js
    
    Add Update Evaluation
    
    * Update evaluations.js
    
    Add Delete Evaluation
    
    * Update evaluations.js
    
    Export Controller Functions
    
    * StudentWorkspace_header
    
    Add studentWorkspace header
    
    * Create dashboard.js
    
    Fix hanging route and folder size error suppression
    
    * Update dashboard.js
    
    Optimize User Update controller
    
    * Update dashboard.js
    
    Clean up redundant code and improve code style
    
    * upload form
    
    Add upload form
    
    * DashboardPage.jsx
    
    create
    
    * dashboardPage.jsx
    
    dashboardpage update
    
    * Add submissions
    
    Add submissions table and feedback loop
    
    * Create User.js
    
    Fix validation constraints for pre-hashed fields
    
    * dashboard page.jsx
    
    dashboard page updated
    
    * Update User.js
    
    edit code
    
    * dashboardpage.jsx
    
    dashboardpage update
    
    * Update User.js
    
    Implement pre-update password hashing support
    
    * Add timeline
    
    Add Progress timeline
    
    * Update User.js
    
    edit
    
    ---------
    
    Co-authored-by: Nirojanpiraba2003 <nirojanpiraba@gmail.com>
    Co-authored-by: FathimaJizan <begumjizan@gmail.com>
    Co-authored-by: Samitha Lakshan Wijesooriya <126401081+samitha2119@users.noreply.github.com>

---
+++ server/src/models/User.js
@@ -0,0 +1,27 @@
+// Hash password before saving (Runs on .save() and .create())
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('passwordHash')) return next();
+    const salt = await bcrypt.genSalt(12);
+    this.passwordHash = await bcrypt.hash(this.passwordHash, salt);
+    next();
+});
+
+// NEW: Hash password before updating (Runs on findByIdAndUpdate / findOneAndUpdate)
+userSchema.pre('findOneAndUpdate',async function (next) {
+    const update = this.getUpdate();
+     con
+    
+    // Check if passwordHash is being modified in this update payload
+    if (update.passwordHash) {
+        try {
+            const salt = await bcrypt.genSalt(12);
+            update.passwordHash = await bcrypt.hash(update.passwordHash, salt);
+        } catch (err) {
+            return next(err);
+        }
+    }
+    next();
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function (candidatePassword)
